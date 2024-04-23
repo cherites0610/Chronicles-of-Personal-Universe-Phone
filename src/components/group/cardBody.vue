cdfbf3020218c34e4ad9042c6113d28ccc48cc2a
cherites0610 <cherites0610@gmail.com>
2024-04-23 18:11:43 +0800

    feat:群組功能框架完成

---
+++ src/components/group/cardBody.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card-body">
+        <h2 class="card-title">{{ props.group.name }}</h2>
+        <span class="text-lg font-semibold">成員:</span>
+        <span v-for="item in props.group.members"> {{ item }}</span>
+        <div class="avatar-group -space-x-6 rtl:space-x-reverse">
+            <div v-for="index in props.group.members" class="avatar">
+                <div class="size-7">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['group'])
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/group.vue
+++ src/views/group.vue
@@ -1,13 +1,55 @@
 <template>
-    <div>
-        群組
+    <div class=" flex flex-col gap-3 w-full p-4">
+        <div v-for="index in Math.ceil(groups.length / 2)" class="flex gap-3 flex-row">
+            <div class="card basis-1/2 h-80 bg-cyan-200 shadow-xl">
+                <cardBody :group="groups[(index - 1) * 2]" />
+            </div>
+            <div v-if="isShow(index)" class="card basis-1/2 h-80 bg-stone-200 shadow-xl">
+                <cardBody :group="groups[index * 2 - 1]" />
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+import cardBody from '@/components/group/cardBody.vue';
 
+const isShow = (index) => {
+    if (groups.length % 2 == 0) {
+        return true
+    } else if (Math.ceil(index * 2) > groups.length) {
+        return false
+    } else {
+        return true;
+    }
+}
+
+const groups = [
+    {
+        name: '第一組',
+        members: ['羽毛', '47', '猴子']
+    },
+    {
+        name: '第二組',
+        members: ['子芸', '寶利發', '管樂大師']
+    },
+    {
+        name: '第三組',
+        members: ['123', '456', '789']
+    },
+    {
+        name: '第四組',
+        members: ['123', '456', '789']
+    },
+    {
+        name: '第五組',
+        members: ['123', '456', '789']
+    },
+    {
+        name: '第六組',
+        members: ['123', '456', '789']
+    }
+]
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

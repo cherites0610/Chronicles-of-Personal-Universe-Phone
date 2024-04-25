b34298ec06d3400ddbcb0c54697f1619f2196467
cherites0610 <cherites0610@gmail.com>
2024-04-26 00:01:02 +0800

    fix:賬戶資料跑板修復v1

--- src/views/account.vue
+++ src/views/account.vue
@@ -7,21 +7,26 @@
             </div>
         </div>
 
-        <div class=" h-56 w-80 card-bordered card">
-            <p class="w-full text-center card-title">賬戶資料</p>
-            <div class="card-body p-5">
-                <div>賬號:羽毛</div>
-                <div>暱稱:毛毛毛毛毛毛</div>
-                <div>郵箱:maomao@mcu.edy.tw.com</div>
-                <div>密碼:**********</div>
+        <div class="felx flex-wrap justify-center content-center h-56 w-80 card-bordered card">
+            <p class=" w-20 text-center card-title">賬戶資料</p>
+            <div class=" card-body p-5">
+                <div>賬號:{{ user.account }}</div>
+                <div>暱稱:{{ user.name }}</div>
+                <div>郵箱:{{ user.email }}</div>
+                <div>密碼:{{ user.password }}</div>
             </div>
         </div>
     </div>
 
 </template>
 
-<script setup lang="ts">
-
+<script setup>
+const user = ref({
+    account: '未讀取',
+    name: '未讀取',
+    email: '未讀取',
+    password: '未讀取'
+})
 </script>
 
 <style scoped></style>

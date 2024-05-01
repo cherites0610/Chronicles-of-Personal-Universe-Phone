a80ba138624e1263acded0855595c5114cad4857
MonkeyKao <sunnylovemonkey@gmail.com>
2024-05-02 00:42:18 +0800

    Modify : 登入css修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,31 +1,50 @@
 <template>
-  <div class="login">
-    <span class="label-text">帳號</span>
+  <div class="mt-10 flex flex-col gap-3 w-full p-4">
+    <h3 class="label-text text-center text-xl">Login</h3>
+
+    <span class="label-text items-center p-4 flex h-5"
+      ><ph-user :size="32" />
+
+      帳號</span
+    >
     <label class="input input-bordered flex items-center gap-2">
       <input type="text" v-model="form.account" />
     </label>
-    <span class="label-text">密碼</span>
+
+    <span class="label-text items-center p-4 flex h-5"
+      ><ph-lock-key :size="32" />密碼</span
+    >
     <label class="input input-bordered flex items-center gap-2">
       <input type="text" v-model="form.password" />
     </label>
 
-    <button class="btn btn-outline btn-login" @click="() => say('登入成功')">
-      登入
-    </button>
+    <div class="flex justify-center">
+      <button
+        class="btn btn-primary w-60"
+        @click="handleLoginClick('登入成功')"
+      >
+        登入
+      </button>
+    </div>
+
+    <div class="flex items-center justify-center gap-4">
+      <h1 class="text">還沒有帳號嗎?</h1>
+      <button class="btn btn-active btn-link">點此註冊</button>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
-
-const form = reactive({
+const form = ref({
   account: "",
   password: "",
 });
 
-function say(message) {
-  if (form.account === "sunny" && form.password === "1111") {
-    alert(message);
+function handleLoginClick(message) {
+  if (form.value.account === "sunny" && form.value.password === "06141111") {
+    console.info(message);
+  } else {
+    console.warn("登入失敗");
   }
 }
 </script>
